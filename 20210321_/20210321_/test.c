4fac931fc67329caf70e437383f8e2e14ff07fd3
240428037 <240428037@qq.com>
2021-03-22 11:32:58 +0800

    提交代码

---
+++ 20210321_/20210321_/test.c
@@ -0,0 +1,185 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+
+////最大公约数
+////给定两个数 求这两个数的最大公约数
+////辗转相除法
+//int main()
+//{
+//	int m = 0;
+//	int n = 0;
+//	int r = 0;
+//	scanf("%d%d", &m, &n);
+//	while (r=m%n)
+//	{
+//		m = n;
+//		n = r;
+//	}
+//	printf("%d\n", n);
+//	return 0;
+//}
+
+////打印闰年
+////打印1000年到2000年之间的闰年
+//int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	for (year = 1000; year <= 2000;year++)
+//	{
+//		//判断year是否为闰年
+//		//1.能被4整除并且不能被100整除是闰年
+//		//2.能被400整除是如年
+//		
+//		if (year % 4 == 0 && year % 100 != 0)
+//		{
+//			printf("%d " , year);
+//			count++;
+//		}
+//		else if (year%400==0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//	} 
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+
+
+//打印闰年
+//打印1000年到2000年之间的闰年
+//int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		//判断year是否为闰年
+//		//1.能被4整除并且不能被100整除是闰年
+//		//2.能被400整除是如年
+//
+//		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+
+////打印素数
+////写一个代码 打印100-200之间的素数
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否为素数
+//		//素数判断的规则
+//		//1.试除法
+//		//产生2->i-1
+//		int j = 0;
+//		for (j = 2; j < i; j++)
+//		{
+//			if (i%j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		//1.break;
+//		//2.循环走完
+//		if (j == i)
+//		{
+//			printf("%d ", i);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+//#include <math.h>
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		//判断i是否为素数
+//		//素数判断的规则
+//		//1.试除法
+//		//产生2->i-1
+//		int j = 0;
+//		for (j = 2; j < sqrt(i); j++)
+//		{
+//			if (i%j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		//1.break;
+//		//2.循环走完
+//		if (j>sqrt(i))
+//		{
+//			printf("%d ", i);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+
+//#include <math.h>
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 101; i <= 200; i+=2)
+//	{
+//		//判断i是否为素数
+//		//素数判断的规则
+//		//1.试除法
+//		//产生2->i-1
+//		int j = 0;
+//		for (j = 2; j < sqrt(i); j++)
+//		{
+//			if (i%j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		//走到此处
+//		//1.break;
+//		//2.循环走完 写第二种情况的if
+//		if (j>sqrt(i))
+//		{
+//			printf("%d ", i);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+
+
+
+int main()
+{
+
+
+
+	return 0;
+}
+
+
+
+
